1434758f7411035b79242a0522ad06510e6e152c
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2019-04-14 23:25:48 +0200

    Fix: Auto-Created Index Name Too Long

--- database/migrations/2019_04_01_02_create_aliases_table.php
+++ database/migrations/2019_04_01_02_create_aliases_table.php
@@ -19,29 +19,18 @@ class CreateAliasesTable extends Migration
                 $table->integer('id')->unsigned()->autoIncrement();
                 $table->string('source_username', 64);
                 $table->string('source_domain', 190);
-                $table->string('source')->virtualAs('CONCAT(`source_username`, "@", `source_domain`)')->nullable()->index();
                 $table->string('destination_username', 64);
                 $table->string('destination_domain', 190);
-                $table->string('destination')->virtualAs('CONCAT(`destination_username`, "@", `destination_domain`)')->nullable()->index();
                 $table->boolean('enabled')->default(true);
                 $table->timestamps();
-                $table->unique(['source_username', 'source_domain', 'destination_username', 'destination_domain']);
+                $table->unique(['source_username', 'source_domain', 'destination_username', 'destination_domain'], 'source_destination');
                 $table->foreign('source_domain')->references('domain')->on('domains');
             });
         } else {
-            if (Schema::hasColumn('aliases', 'source_username')
-                && Schema::hasColumn('aliases', 'source_domain')
-                && Schema::hasColumn('aliases', 'destination_username')
-                && Schema::hasColumn('aliases', 'destination_domain')) {
-                    // There is an existing mail server setup, so just add some columns and indexes.
-                    Schema::table('aliases', function(Blueprint $table) {
-                        $table->string('source')->virtualAs('CONCAT(`source_username`, "@", `source_domain`)')->nullable()->index();
-                        $table->string('destination')->virtualAs('CONCAT(`destination_username`, "@", `destination_domain`)')->nullable()->index();
-                        $table->timestamps();
-                    });
-            } else {
-                // The database schema is not compatible with Post Office.
-            }
+            // There is an existing mail server setup, so just add some columns.
+            Schema::table('aliases', function(Blueprint $table) {
+                $table->timestamps();
+            });
         }
     }
 
@@ -52,10 +41,8 @@ class CreateAliasesTable extends Migration
      */
     public function down()
     {
-        // Just drop the Post Office specific columns and indexes, but keep the basic mail server setup.
+        // Just drop the Post Office specific columns, but keep the basic mail server setup.
         Schema::table('aliases', function (Blueprint $table) {
-            $table->dropIndex(['source', 'destination']);
-            $table->dropColumn(['source', 'destination']);
             $table->dropTimestamps();
         });
     }
