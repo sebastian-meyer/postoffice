65d4e078f33aa3a6c17df9a5be8eadf14f0b10c2
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2019-04-14 23:08:34 +0200

    Update DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            DomainsTableSeeder::class,
+            AccountsTableSeeder::class,
+            AliasesTableSeeder::class,
+            TlsPoliciesTableSeeder::class,
+        ]);
     }
 }
