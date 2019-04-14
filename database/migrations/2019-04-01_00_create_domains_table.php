0ff331e695367e39785be81878dd7f5ccce9823a
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2019-04-14 23:06:22 +0200

    Add New Domain Model

---
+++ database/migrations/2019-04-01_00_create_domains_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDomainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (! Schema::hasTable('domains')) {
+            // There is no mail server setup present. We'll create all necessary tables.
+            Schema::create('domains', function (Blueprint $table) {
+                $table->integer('id')->unsigned()->autoIncrement()->primary();
+                $table->string('domain', 190)->unique();
+                $table->timestamps();
+            });
+        } else {
+            // There is an existing mail server setup, so just add some columns.
+            Schema::table('domains', function(Blueprint $table) {
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Just drop the Post Office specific columns, but keep the basic mail server setup.
+        Schema::table('domains', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+    }
+}
