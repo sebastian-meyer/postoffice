b8870b78d76a544c87fba095d70b9800423b5c88
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2020-01-29 22:52:13 +0100

    Remove Pusher Support

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -11,7 +11,7 @@ return [
     | framework when an event needs to be broadcast. You may set this to
     | any of the connections defined in the "connections" array below.
     |
-    | Supported: "pusher", "redis", "log", "null"
+    | Supported: "redis", "log", "null"
     |
     */
 
@@ -29,17 +29,6 @@ return [
     */
 
     'connections' => [
-
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-            ],
-        ],
 
         'redis' => [
             'driver' => 'redis',
